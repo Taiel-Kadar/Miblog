c44c86a3f1b5c2f790dc6b1516066c48facdbb6a
Taiel-Kadar <taikdr20@gmail.com>
2020-08-20 00:39:03 -0300

    Deployed, added requirements.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%(y)@lp@uiyec6j=#hx3th)ivnl47+t_dk7^2xy@3(5#t@h&=+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
